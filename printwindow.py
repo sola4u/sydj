d43eac7fd7faf4623d2fb349eda246509d3ac518
sola <hfchun@vip.qq.com>
2018-10-17 16:13:08 +0800

    'v0.0.101716'

--- printwindow.py
+++ printwindow.py
@@ -14,36 +14,102 @@ import datetime
 import print_modle
 import sys
 
-
 class PrintWindow(QWidget):
 
-    def __init__(self,id):
+    def __init__(self, id):
         super(PrintWindow, self).__init__()
+        self.setFixedSize(810, 600)
+        self.printer = QPrinter()
+        self.printer.setPageSize(QPrinter.A4)
+        self.id = id
+        self.set_ui()
+
+    def  set_ui(self):
+        self.layout = QVBoxLayout()
+
+        self.print_bnt = QPushButton("打印")
+        self.print_bnt2 = QPushButton("仅打印数据")
+        self.close_bnt = QPushButton('关闭')
+        self.to_page2_bnt = QPushButton("查看第二联")
+        self.print_bnt.clicked.connect(self.print_page1)
+        self.print_bnt2.clicked.connect(self.print_page1_data)
+        self.close_bnt.clicked.connect(self.close_page)
+        self.to_page2_bnt.clicked.connect(self.to_page2)
+
+        self.paintarea = PaintArea(self.id)
+
+        self.bnt_layout = QHBoxLayout()
+        self.bnt_layout.addStretch(4)
+        self.bnt_layout.addWidget(self.to_page2_bnt)
+        self.bnt_layout.addWidget(self.print_bnt)
+        self.bnt_layout.addWidget(self.print_bnt2)
+        self.bnt_layout.addWidget(self.close_bnt)
+
+        self.bnt_layout2 = QWidget()
+        self.bnt_layout2.setLayout(self.bnt_layout)
+
+        self.layout.addWidget(self.bnt_layout2)
+        self.layout.addWidget(self.paintarea)
+
+        self.scroll = QScrollArea(self)
+        self.scroll.setAutoFillBackground(True)
+        self.scroll.setMinimumSize(800, 600)
+        self.scroll.setWidgetResizable(True)
+        self.scroll_bar = self.scroll.verticalScrollBar()
+
+        self.layout2 = QWidget()
+        self.layout2.setLayout(self.layout)
+
+        self.scroll.setWidget(self.layout2)
+
+        self.layout3 = QVBoxLayout()
+        self.layout3.addWidget(self.scroll)
+
+        self.setLayout(self.layout3)
+
+    def print_page1(self):
+        # dialog = QPrintDialog(self.printer, self)
+        # if not dialog.exec_():
+        #     return
+        a = PaintArea2(self.id)
+
+    def print_page1_data(self):
+        pass
+
+    def close_page(self):
+        self.close()
+
+    def to_page2(self):
+        pass
+
+class PaintArea(QWidget):
+
+    def __init__(self,id):
+        super(PaintArea, self).__init__()
         self.id= str(id)
-        self.resize(800, 600)
+        self.setFixedSize(760, 1080)
         self.printer = QPrinter()
         self.printer.setPageSize(QPrinter.A4)
         # self.drawText()
-        self.bnt = QPushButton("print")
-        self.layout = QHBoxLayout()
-        self.layout.addWidget(self.bnt)
-        self.setLayout(self.layout)
 
     def paintEvent(self, event):
         # dialog = QPrintDialog(self.printer, self)
         # if not dialog.exec_():
             # return
-        qp = QPainter(self.printer)
+        # qp = QPainter(self.printer)
+        map = QPixmap()
+        qp = QPainter(map)
         qp.begin(self)
         self.drawText(event, qp)
         qp.end()
+        # map.save('a.png')
 
     def drawText(self,event, qp):
     # def drawText(self):
-        # dialog = QPrintDialog(self.printer, self)
-        # if not dialog.exec_():
-            # return
-        # qp = QPainter(self.printer)
+        dialog = QPrintDialog(self.printer, self)
+        if not dialog.exec_():
+            return
+        qp = QPainter(self.printer)
         self.db = DataBase()
         self.db.cur.execute("select * from death_info where  serial_number = '%s'"%(self.id))
         self.rslt = self.db.cur.fetchone()
@@ -63,15 +129,16 @@ class PrintWindow(QWidget):
             education = self.list_dic.education_list[self.rslt[13]]
             occupation = self.list_dic.occupation_list[self.rslt[14]]
             death_location = self.list_dic.death_location_list[self.rslt[19]]
-            diagnost_department = self.list_dic.diagnost_department_list[self.rslt[39]]
-            diagnost_method = self.list_dic.diagnost_method_list[self.rslt[40]]
+            diagnost_department = self.list_dic.diagnost_department_list[self.rslt[40]]
+            diagnost_method = self.list_dic.diagnost_method_list[self.rslt[41]]
             rslt_list = [address,self.rslt[1],self.rslt[4],self.rslt[5],gender,race,id_class,self.rslt[9],
                         self.rslt[11],marriage,self.change_date(self.rslt[10]),education,occupation,
                         self.change_date(self.rslt[21]),death_location,'否',self.rslt[20],self.rslt[17],
                         self.rslt[15],self.rslt[22], self.rslt[23],self.rslt[24],self.rslt[25],str(self.rslt[26])+self.rslt[27],
                         self.rslt[28],str(self.rslt[29])+self.rslt[30], self.rslt[31], str(self.rslt[32])+self.rslt[33],
                         self.rslt[34], str(self.rslt[35])+self.rslt[36],self.rslt[37],diagnost_department,diagnost_method,
-                        self.change_date(self.rslt[53]),self.rslt[52],self.rslt[38],0,0,0,0,0,0,0,0]
+                        self.change_date(self.rslt[44]),self.rslt[38],self.rslt[39],self.rslt[48],self.rslt[49],self.rslt[50],
+                        self.rslt[52],self.rslt[51],self.rslt[53],self.change_date(self.rslt[54])]
             for i in range(len(rslt_list)):
                 print(i, rslt_list[i])
 
@@ -274,22 +341,64 @@ class PrintWindow(QWidget):
         qp.drawRect(494,x,84,36)
         qp.drawRect(578,x,150,36)
         qp.drawText(40,x,219,30,Qt.AlignCenter,'(以下由编码人员填写)根本死亡原因:')
+        qp.drawText(259,x,235,30,Qt.AlignCenter,rslt_list[34])
         qp.drawText(494,x,84,30,Qt.AlignCenter,'ICD编码:')
+        qp.drawRect(578,x,150,36)
+        qp.drawText(578,x,150,30,Qt.AlignCenter,rslt_list[35])
 
         x += 36
         qp.setFont(QFont('宋体', 14,QFont.Bold))
-        qp.drawText(40,x,728,40,Qt.AlignCenter,'死亡调查记录')
+        qp.drawText(40,x,688,40,Qt.AlignCenter,'死亡调查记录')
         qp.setFont(QFont('宋体', 9))
 
         x += 40
-        qp.drawRect(40,x,700,118)
+        qp.drawRect(40,x,688,128)
         qp.drawText(40,x,180,36,Qt.AlignCenter,'死者生前病史及症状体征:')
-        research_len = len(rslt_list[34])
-        for i in range(research_len%70):
-            qp.drawText(60,x+36*(1+i),700,36,Qt.AlignLeft,rslt_list[34][i*70:(i+1)*70])
-        qp.drawText(438,x+80,250,36,Qt.AlignCenter,'以上情况属实，被调查者签字:')
+        research_len = len(rslt_list[36])
+        for i in range(research_len%55):
+            qp.drawText(60,x+30*(1+i),700,36,Qt.AlignLeft,rslt_list[36][i*50:(i+1)*50])
+        qp.drawText(438,x+110,250,36,Qt.AlignLeft,'以上情况属实，被调查者签字:')
 
+        x += 128
+        qp.drawRect(40,x,75,36)
+        qp.drawRect(115,x,100,36)
+        qp.drawRect(215,x,44,36)
+        qp.drawRect(259,x,55,36)
+        qp.drawRect(314,x,80,36)
+        qp.drawRect(394,x,100,36)
+        qp.drawRect(494,x,84,36)
+        qp.drawRect(578,x,150,36)
+        qp.drawText(40,x,75,20,Qt.AlignCenter,'被调查者')
+        qp.drawText(40,x,75,50,Qt.AlignCenter,'姓名')
+        qp.drawText(115,x,100,30,Qt.AlignCenter,rslt_list[37])
+        qp.drawText(215,x,44,20,Qt.AlignCenter,'与死者')
+        qp.drawText(215,x,44,50,Qt.AlignCenter,'关系')
+        qp.drawText(259,x,55,30,Qt.AlignCenter,rslt_list[38])
+        qp.drawText(314,x,80,20,Qt.AlignCenter,'联系')
+        qp.drawText(314,x,80,50,Qt.AlignCenter,'电话')
+        qp.drawText(394,x,100,30,Qt.AlignCenter,rslt_list[39])
+        qp.drawText(494,x,84,20,Qt.AlignCenter,'联系地址')
+        qp.drawText(494,x,84,50,Qt.AlignCenter,'或工作单位')
+        qp.drawText(578,x,150,30,Qt.AlignCenter,rslt_list[40])
 
+        x +=36
+        qp.drawRect(40,x,75,36)
+        qp.drawRect(115,x,199,36)
+        qp.drawRect(314,x,80,36)
+        qp.drawRect(394,x,100,36)
+        qp.drawRect(494,x,84,36)
+        qp.drawRect(578,x,150,36)
+        qp.drawText(40,x,75,30,Qt.AlignCenter,'死因推断')
+        qp.drawText(115,x,199,30,Qt.AlignCenter,rslt_list[41])
+        qp.drawText(314,x,80,30,Qt.AlignCenter,'被调查者签名')
+        qp.drawText(494,x,84,30,Qt.AlignCenter,'调查日期')
+        qp.drawText(578,x,150,30,Qt.AlignCenter,rslt_list[42])
+
+        x += 36
+        qp.drawText(40,x,688,30,Qt.AlignLeft,'注：①此表填写范围为在家、养老服务机构、其他场所正常死亡镇；②被调查者应为死者近亲或知情人；③调查时应出具以下资料:')
+
+        x += 20
+        qp.drawText(40,x,688,30,Qt.AlignLeft,'被调查者有效身份证件，居住地居委会或村委会证明，死者身份证和/或户口簿、生前病史卡.')
 
 
     def change_date(self,a):  #time stamp to yyyymmdd
@@ -297,120 +406,20 @@ class PrintWindow(QWidget):
         return str(date.year)+'年'+ str(date.month) +'月'+str(date.day)+'日'
 
 
-class PrintWindow2(QWidget):
+class PaintArea2(PaintArea):
 
     def __init__(self,id):
-        super(PrintWindow2, self).__init__()
+        super(PaintArea2,self).__init__()
         self.id = id
-        self.printer = QPrinter()
-        self.printer.setPageSize(QPrinter.A4)
-        # self.setFixedSize(800,600)
-        self.resize(1200,800)
-        self.setWindowTitle("打印")
-        self.set_ui()
 
-    def set_ui(self):
-        self.db = DataBase()
-        self.db.cur.execute("select * from death_info where  serial_number = '%s'"%(self.id))
-        self.rslt = self.db.cur.fetchone()
-        self.db.con.close()
-        self.list_dic = Choice_Dic()
-        for i in range(len(self.rslt)):
-            print(i, self.rslt[i])
-        if self.rslt:
-            province = address_dic.province_dic[self.rslt[1][:2]]
-            city = address_dic.city_dic[self.rslt[1][:2]][self.rslt[1][:4]]
-            county = address_dic.county_dic[self.rslt[1][:4]][self.rslt[1][:6]]
-            address = province + city + county
-            gender = self.list_dic.gender_list[self.rslt[6]]
-            race = self.list_dic.race_list[self.rslt[7]]
-            id_class = self.list_dic.id_class_list[self.rslt[8]]
-            marriage = self.list_dic.marriage_list[self.rslt[12]]
-            education = self.list_dic.education_list[self.rslt[13]]
-            occupation = self.list_dic.occupation_list[self.rslt[14]]
-            death_location = self.list_dic.death_location_list[self.rslt[19]]
-            diagnost_department = self.list_dic.diagnost_department_list[self.rslt[39]]
-            diagnost_method = self.list_dic.diagnost_method_list[self.rslt[40]]
-            a = print_modle.html_page1
-            html_page2 = a.format(address,self.rslt[1],self.rslt[4],self.rslt[5],gender,race,id_class,self.rslt[9],
-                        self.rslt[11],marriage,self.change_date(self.rslt[10]),education,occupation,
-                        self.change_date(self.rslt[21]),death_location,'否',self.rslt[20],self.rslt[17],
-                        self.rslt[15],self.rslt[22], self.rslt[23],self.rslt[24],self.rslt[25],str(self.rslt[26])+self.rslt[27],
-                        self.rslt[28],str(self.rslt[29])+self.rslt[30], self.rslt[31], str(self.rslt[32])+self.rslt[33],
-                        self.rslt[34], str(self.rslt[35])+self.rslt[36],self.rslt[37],diagnost_department,diagnost_method,
-                        self.change_date(self.rslt[53]),self.rslt[38],0,0,0,0,0,0,0,0)
-            html_page1 = print_modle.test_page
-        else:
-            text = '''<h1>无此条记录</h1>
-            '''
-        print(html_page1)
-        self.close_bnt = QPushButton("关闭")
-        self.close_bnt.clicked.connect(self.close_click)
-        self.print_bnt1 = QPushButton("打印第二联")
-        self.print_bnt2 = QPushButton('打印')
-        self.print_bnt2.clicked.connect(lambda:self.print_page1(html_page1))
-        self.print_bnt3 = QPushButton('无模版打印')
-
-        self.bnt_layout = QHBoxLayout()
-        self.bnt_layout.addWidget(self.print_bnt1)
-        self.bnt_layout.addWidget(self.print_bnt2)
-        self.bnt_layout.addWidget(self.close_bnt)
-
-        self.bnt_layout2 = QWidget()
-        self.bnt_layout2.setLayout(self.bnt_layout)
-
-        self.content = QTextEdit()
-        self.content.insertHtml(html_page1)
-
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.bnt_layout2)
-        self.layout.addWidget(self.content)
-        self.setLayout(self.layout)
-
-    def close_click(self):
-        self.close()
-
-    def print_page1(self, htmltxt):
+    def paintEvent(self, event):
         dialog = QPrintDialog(self.printer, self)
-        if dialog.exec_():
-            document = QTextDocument()
-            document.setHtml(htmltxt)
-            document.print_(self.printer)
-        self.close()
-
-    def print_page1_4a4(self):
-        dialog = QPrintDialog(self.printer, self)
-        if dialog.exec_():
-            document = QTextDocument()
-            document.setHtml(html_page1_4a4)
-            document.print_(self.printer)
-        self.close()
-
-    def print_page2(self):
-        dialog = QPrintDialog(self.printer, self)
-        if dialog.exec_():
-            document = QTextDocument()
-            document.setHtml(html_page1_4a4)
-            document.print_(self.printer)
-        self.close()
-
-    def print_page2_4a4(self):
-        dialog = QPrintDialog(self.printer, self)
-        if dialog.exec_():
-            document = QTextDocument()
-            document.setHtml(html_page1_4a4)
-            document.print_(self.printer)
-        self.close()
-
-    def to_page2(self):
-        pass
-
-    def to_page1(self):
-        pass
-
-    def change_date(self,a):  #time stamp to yyyymmdd
-        date = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=a)
-        return str(date.year)+'年'+ str(date.month) +'月'+str(date.day)+'日'
+        if not dialog.exec_():
+            return
+        qp = QPainter(self.printer)
+        qp.begin(self)
+        self.drawText(event, qp)
+        qp.end()
 
 
 if __name__ == '__main__':
